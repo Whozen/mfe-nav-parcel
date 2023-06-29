2ba2fb82c34923ee748913d59c8670416960df99
Ujjen Baniya <ujjenbaniya@Egens-MacBook-Pro.local>
2023-06-29 16:10:01 -0500

    feat: scaffolding navbar parcel

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div class="topnav">
+    <a class="active" href="/#/">App 1</a>
+    <a href="/#/second">App 2</a>
+  </div>
+
+  <div style="padding-left:16px">
+    <h2>This is Micro-Frontend Parcel</h2>
+  </div>
+</template>
+
+<script>
+//import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
